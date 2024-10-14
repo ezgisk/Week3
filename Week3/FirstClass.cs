a367db655511269ea19cef2c6c8a4266f2b3b6b0
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-14 16:26:08 -0700

    Fields added.

--- Week3/FirstClass.cs
+++ Week3/FirstClass.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Week3
 {
     public class Person
     {
         // Creating person class and fields
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public DateTime Birthday { get; set; }
+        private string name;
+        private string surname;
+        private DateTime birthday;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
 
     }
 }
