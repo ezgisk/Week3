400640e01ff11cfbbd4ef71defa6c6d0c19559b0
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-14 16:11:51 -0700

    Add project files.

---
+++ Week3/FirstClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    public class Person
+    {
+        // Creating person class and fields
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Week3;
+
+Person student = new Person();// Creating student object
+
+student.Name = "Ezgi";
+student.Surname = "Gecimli";
+student.Birthday = new DateTime(1997, 4, 12);
+
+Console.WriteLine($"Ogrencinin Adi : {student.Name} Ogrencinin Soyadi : {student.Name} Ogrencinin Dogum gunu : {student.Birthday}");
+
+Person teacher = new Person(); // Creating teacher object
+teacher.Name = "Gorkem";
+teacher.Surname = "Gecimli";
+teacher.Birthday = new DateTime(1998, 6, 8);
+
+Console.WriteLine($"Ogretmenin Adi : {teacher.Name} Ogretmenin Soyadi : {teacher.Name} Ogretmenin Dogum gunu : {teacher.Birthday}");
+
+Console.ReadKey();  
